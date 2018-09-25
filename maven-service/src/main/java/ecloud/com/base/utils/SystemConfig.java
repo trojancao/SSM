52cb62f7edd04f96b41d13386170db094ef708c7
trojancao <trojancao@163.com>
2018-09-25 10:55:02 +0800

    Update SystemConfig.java

--- maven-service/src/main/java/ecloud/com/base/utils/SystemConfig.java
+++ maven-service/src/main/java/ecloud/com/base/utils/SystemConfig.java
@@ -19,4 +19,51 @@ public class SystemConfig {
 		return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest(); 
 		
 	}
+	public static String getIpAddr() {
+		//获取当前的request 多例的
+		ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		if(attributes == null) {
+			return "";
+		}
+		HttpServletRequest request = attributes.getRequest();
+		if(request == null) {
+			return "";
+		}
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_CLIENT_IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("X-Real-IP");  
+        }  
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+			if (ip != null && (ip.equals("127.0.0.1") || ip.equals("0:0:0:0:0:0:0:1"))) {
+				//根据网卡取本机配置的IP 
+				InetAddress inet = null;
+				try {
+					inet = InetAddress.getLocalHost();
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				}
+				ip = inet.getHostAddress();
+			}
+		}
+		// 多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割    
+		if (ip != null && ip.length() > 15) {
+			if (ip.indexOf(",") > 0) {
+				ip = ip.substring(0, ip.indexOf(","));
+			}
+		}
+		return ip;
+	}
 }
